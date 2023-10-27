398612ae7f06b8ccd1a289f533ea4e6f0e20b122
Gurmehar <gurmeharmgn@gmail.com>
2023-10-27 22:13:51 +0530

    updated app.js and app.css
    Final fixes in the text-utils app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Form from "./components/Form";
+import About from "./components/About";
+import { useState } from "react";
+import Alert from "./components/Alert";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
+// let name="harry";
 function App() {
+  const [mode, setmode] = useState("light");
+  const [btntext, setbtntext] = useState("dark");
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+  const togglemode = () => {
+    if (mode === "light") {
+      setbtntext("light");
+      setmode("dark");
+      document.body.style.backgroundColor = "#1c4873";
+      showAlert("Dark mode has been enabled", "success");
+      document.title = "Textutils - Dark Mode";
+    } else {
+      setbtntext("dark");
+      setmode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+      document.title = "Textutils - Light Mode";
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar
+          title="TextUtils"
+          aboutext="About Us"
+          mode={mode}
+          btntext={btntext}
+          togglemode={togglemode}
+        />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/" element={<Form
+                showAlert={showAlert}
+                heading="Try Textutils - Word Counter  , Character Counter , Remove Extra spaces"
+                mode={mode}
+              />} />
+              
+            
+            <Route exact path="/about" element={<About />} />
+              
+          
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
